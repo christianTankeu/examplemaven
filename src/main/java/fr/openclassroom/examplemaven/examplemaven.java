e8900c820131a15b2c417845d8c9502da32c0dd1
ghost <tankeuchristian1@gmail.com>
2022-06-24 17:20:19 +0100

    Base Java

---
+++ src/main/java/fr/openclassroom/examplemaven/examplemaven.java
@@ -0,0 +1,14 @@
+package fr.openclassroom.examplemaven;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+public class examplemaven {
+
+	public static void main(String[] args) {
+		ImagePlus imp = IJ.openImage();
+		imp.show();
+
+	}
+
+}
